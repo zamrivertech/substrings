44655b0163ba8f347c1d35f28d9f9a7bf2a6e820
zamrivertech <mohamadtasul8000@gmail.com>
2026-07-10 23:47:27 +0200

    Write and save current pseucode for substring logic

---
+++ substrings.rb
@@ -0,0 +1,31 @@
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings(word = "", array)
+  
+
+  # Take the word (Case Insensitive)
+
+    # if word comes empty like "", return count of all words
+    # NOT FINISHED
+    
+    # if word comes not empty, return matching by at least 3 letters for match
+      # like below and low OR howdy with how
+    
+      # Look matching for word or words 
+        # Take the first original word
+        # Look for word inside array with a finder
+        # if found, increase count, using that hash count method
+        # what about other matching words
+        # use include
+        # Write more pseudocode to only spend
+        # imlementing code for 10 minutes and
+        # getting correct results and avoid bugs
+            
+      # For final hash method count and return
+      # Please review TOP lesson    
+  
+
+end
+
+substrings("below", dictionary)
+
