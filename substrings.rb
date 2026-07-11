6b5abbf18f7fdd1e260ca1e5083f85f0856cd042
zamrivertech <mohamadtasul8000@gmail.com>
2026-07-11 09:03:09 +0200

    Implement word match count partial of the project

--- substrings.rb
+++ substrings.rb
@@ -2,16 +2,35 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 
 def substrings(word = "", array)
   
+  hash_count = {}
 
   # Take the word (Case Insensitive)
+  
+  # Use reduce to return hash with count, initialize with Hash.new(0) 
+  # or just test with {0} to see what will happen
 
     # if word comes empty like "", return count of all words
     # NOT FINISHED
-    
-    # if word comes not empty, return matching by at least 3 letters for match
+    # ____________________________________________________________
+    # FOCUS BELOW FOR NOW
+     
+   
+      array.reduce(Hash.new(0)) do |acc,value| 
+      
+        if word.include?(value)
+          acc[value] += 1
+        end
+
+         hash_count = acc
+
+      end
+  
+      p hash_count
+
+    # if word comes not empty, return matching using include
       # like below and low OR howdy with how
-    
       # Look matching for word or words 
+      
         # Take the first original word
         # Look for word inside array with a finder
         # if found, increase count, using that hash count method
