36e0fd849c7abac4ef35f318ab1c3b6eb14956fb
zamrivertech <mohamadtasul8000@gmail.com>
2026-07-12 15:00:50 +0200

    Return word count by hash and sort hash

--- substrings.rb
+++ substrings.rb
@@ -1,7 +1,33 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-def substrings(word, array)
+def substrings(words, array)
+
+  words = words.downcase.split(" ")
+
+  word_count = Hash.new(0)
+
+  words.each do |word|
+    
+    array.each do |item|
+
+      if word.include?(item)
+        # word + " - " + item + " :true"
+        word_count[item] += 1 
+
+      
+      #else
+       
+        # word + " - "+ item + " :false"
+
+      end
+
+    end
+
+  end
   
+  word_count = word_count.sort_by { |k| k}.to_h
+
+  p word_count
 
 end
 
